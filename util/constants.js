a2998e83dc0070cd501b8b13f01c7450a82ce7ce
Dhananjay Shettigar <dhananjay.shettigar2252000@gmail.com>
2022-02-26 23:24:11 +0530

    updated db url

--- util/constants.js
+++ util/constants.js
@@ -1,7 +1,7 @@
 require('dotenv').config();
 
 const PORT = process.env.PORT || 5000;
-const DATABASE_URL = process.env.DATABASE_URL_LOCAL || process.env.DATABASE_URL;
+const DATABASE_URL = process.env.DATABASE_URL || process.env.DATABASE_URL_LOCAL;
 const CODES = {
 	SUCCESS: 'SUCCESS',
 	INTERNAL_SERVER_ERR: 'INTERNAL_SERVER_ERR',
